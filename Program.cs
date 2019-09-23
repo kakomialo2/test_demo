3051acaef4709d8311e22918057d3107a69c97a2
kakomialo2-github <kakomialo2@gmail.com>
2019-09-23 23:24:04 +0300

    pame aekara_1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld000
+{
+    class Program
+    {
+        // something new!!!
+        static void Main(string[] args)
+        {
+            int number=5;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("TAKE " + number + " SHIT!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
